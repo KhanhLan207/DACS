76b8cc3771666f264ec9f812bb766f07fdd3395b
TruongThiTuyetNhi <tuyetnhi290404@gmail.com>
2025-04-20 14:53:35 +0700

    Add Brand Interface

--- TicketBus/Areas/Brand/Controllers/HomeController.cs
+++ TicketBus/Areas/Brand/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TicketBus.Data;
+using TicketBus.Models;
 
 namespace TicketBus.Areas.Brand.Controllers
 {
@@ -18,7 +19,28 @@ namespace TicketBus.Areas.Brand.Controllers
 
         public async Task<IActionResult> Index()
         {
+            // Lấy UserId của người dùng hiện tại
             var userId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(userId))
+            {
+                TempData["Message"] = "Không thể xác định người dùng. Vui lòng đăng nhập lại.";
+                return RedirectToAction("Login", "Account", new { area = "Identity" });
+            }
+
+            // Lấy thông tin hãng xe của người dùng hiện tại
+            var brand = await _context.Brands
+                .AsNoTracking()
+                .FirstOrDefaultAsync(b => b.UserId == userId && b.State == BrandState.HoatDong);
+
+            if (brand == null)
+            {
+                TempData["Message"] = "Hãng xe của bạn chưa được phê duyệt hoặc không tồn tại.";
+            }
+
+            // Truyền thông tin hãng xe vào ViewBag
+            ViewBag.BrandInfo = brand;
+
+            // Lấy danh sách thông báo của hãng xe
             var notifications = await _context.Notifications
                 .Where(n => n.UserId == userId && !n.IsRead)
                 .OrderByDescending(n => n.CreatedDate)
@@ -34,6 +56,7 @@ namespace TicketBus.Areas.Brand.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> MarkNotificationAsRead(int id)
         {
             var notification = await _context.Notifications.FindAsync(id);
@@ -41,6 +64,11 @@ namespace TicketBus.Areas.Brand.Controllers
             {
                 notification.IsRead = true;
                 await _context.SaveChangesAsync();
+                TempData["Message"] = "Đã đánh dấu thông báo là đã đọc.";
+            }
+            else
+            {
+                TempData["Message"] = "Thông báo không tồn tại.";
             }
             return RedirectToAction(nameof(Index));
         }
